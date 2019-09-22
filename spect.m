ffa0eda904affd65132b337302ad261ed0862181
Brian Jordan <bjj17@duke.edu>
2019-09-22 13:35:21 -0400

    Made corresponding file path changes to spect.m

--- spect.m
+++ spect.m
@@ -1,12 +1,14 @@
 %% Spectrogram Audio Signal Analyzing Testing
-
 clear
+fireDir = "fire_sounds/";
+forrestDir = "forrest_sounds/";
+
 figure(1)
-[y,Fs] = audioread('forestfire.mp3');
+[y,Fs] = audioread('fire_sounds/forestfire.mp3');
 y = y(:);
 spectrogram(y)
 
 figure(2)
-[y,Fs] = audioread('Forest-ambience.mp3');
+[y,Fs] = audioread('forrest_sounds/Forest-ambience.mp3');
 y = y(:);
 spectrogram(y)
