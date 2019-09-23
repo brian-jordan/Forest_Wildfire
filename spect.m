b14bb807f7f60cd30c4a8bfb9f1438306ae21a22
Cole Hattler <cwh31@duke.edu>
2019-09-23 13:00:59 -0400

    Added loop to spectrogram script

--- spect.m
+++ spect.m
@@ -1,37 +1,55 @@
 %% Spectrogram Audio Signal Analyzing Testing
 clear
-fireDir = "fire_sounds/";
-forrestDir = "forrest_sounds/";
+fireDir = 'fire_sounds/*.mp3';
+fireFolder = 'fire_sounds/';
+forestDir = 'forrest_sounds/*.mp3';
+forestFolder = 'forrest_sounds/';
 
-figure(1)
+%Spectrograms of All Fire Audio Files
+files = dir(fireDir)
+N = length(files)
+count = 1;
+for i = 1:N
+    name = files(i).name
+    fileDir = ['fire_sounds/' name]
+    [y,Fs] = audioread(fileDir);
+    y = y(:);
+    figure(count);
+    spectrogram(y)
+    title(name,'Interpreter', 'none');
+    count = count+1;
+end
 
-%forestfire.mp3 spectrogram
-[y,Fs] = audioread('fire_sounds/forestfire.mp3');
-y = y(:);
-subplot(2,2,1);
-spectrogram(y)
-title('forestfire.mp3');
+figure(count)
+count = count+1;
 
-%brush_fire.mp3 spectrogram
-[y,Fs] = audioread('fire_sounds/brush_fire.mp3');
-y = y(:);
-subplot(2,2,2);
-spectrogram(y)
-title('brush\_fire.mp3');
-
-%Fire_1.mp3 spectrogram
-[y,Fs] = audioread('fire_sounds/Fire_1.mp3');
-y = y(:);
-subplot(2,2,3);
-spectrogram(y)
-title('Fire\_1.mp3');
-
-%Forest_Fire_1.mp3 spectrogram
-[y,Fs] = audioread('fire_sounds/Forest_Fire_1.mp3');
-y = y(:);
-subplot(2,2,4);
-spectrogram(y)
-title('Forest\_Fire\_1.mp3');
+% %forestfire.mp3 spectrogram
+% [y,Fs] = audioread('fire_sounds/forestfire.mp3');
+% y = y(:);
+% subplot(2,2,1);
+% spectrogram(y)
+% title('forestfire.mp3');
+% 
+% %brush_fire.mp3 spectrogram
+% [y,Fs] = audioread('fire_sounds/brush_fire.mp3');
+% y = y(:);
+% subplot(2,2,2);
+% spectrogram(y)
+% title('brush\_fire.mp3');
+% 
+% %Fire_1.mp3 spectrogram
+% [y,Fs] = audioread('fire_sounds/Fire_1.mp3');
+% y = y(:);
+% subplot(2,2,3);
+% spectrogram(y)
+% title('Fire\_1.mp3');
+% 
+% %Forest_Fire_1.mp3 spectrogram
+% [y,Fs] = audioread('fire_sounds/Forest_Fire_1.mp3');
+% y = y(:);
+% subplot(2,2,4);
+% spectrogram(y)
+% title('Forest\_Fire\_1.mp3');
 
 figure(2)
 [y,Fs] = audioread('forrest_sounds/Forest-ambience.mp3');
