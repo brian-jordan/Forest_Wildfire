39ca663a2ef05099bb85f91ba5ea8dbb2e4dd451
Cole Hattler <cwh31@duke.edu>
2019-09-23 13:34:51 -0400

    Spect now produces frequency and time plots as well

--- spect.m
+++ spect.m
@@ -2,120 +2,66 @@
 clear
 fireDir = 'fire_sounds/*.mp3';
 fireFolder = 'fire_sounds/';
-forestDir = 'forrest_sounds/*.mp3';
-forestFolder = 'forrest_sounds/';
+forestDir = 'forest_sounds/*.mp3';
+forestFolder = 'forest_sounds/';
 
-%Spectrograms of All Fire Audio Files
+%Spectrograms, Frequency Domain and Time Domains of All Fire Audio Files
 files = dir(fireDir)
 N = length(files)
 count = 1;
 for i = 1:N
     name = files(i).name
-    fileDir = ['fire_sounds/' name]
+    fileDir = [fireFolder name]
     [y,Fs] = audioread(fileDir);
     y = y(:);
     figure(count);
+    count = count + 1;
+    
+    %Spectrogram
+    subplot(1,3,1);
     spectrogram(y)
     title(name,'Interpreter', 'none');
-    count = count+1;
+    
+    %Frequency Domain
+    yFFT = fft(y);
+    nfft = length(yFFT);
+    x = Fs./2.*[-1:2./nfft:1-2./nfft];
+    subplot(1,3,2);
+    plot(x,abs(yFFT));
+    title(name,'Interpreter', 'none');
+    xlabel('Frequency (Hz)');
+    ylabel('Magnitude');
+    
+    %Time Domain
+    x = 0:length(y)-1;
+    subplot(1,3,3);
+    plot(x,y);
+    xlabel('Samples');
+    ylabel('Magnitude');
+    title(name,'Interpreter', 'none');
 end
 
-figure(count)
-count = count+1;
+%Spectrograms of All Forest Audio Files
+files = dir(forestDir)
+N = length(files)
+for i = 1:N
+    name = files(i).name
+    fileDir = [forestFolder name]
+    [y,Fs] = audioread(fileDir);
+    y = y(:);
+    figure(count);
+    count = count+1;
+    spectrogram(y)
+    title(name,'Interpreter', 'none');
+end
 
-% %forestfire.mp3 spectrogram
-% [y,Fs] = audioread('fire_sounds/forestfire.mp3');
-% y = y(:);
-% subplot(2,2,1);
-% spectrogram(y)
-% title('forestfire.mp3');
-% 
-% %brush_fire.mp3 spectrogram
-% [y,Fs] = audioread('fire_sounds/brush_fire.mp3');
-% y = y(:);
-% subplot(2,2,2);
-% spectrogram(y)
-% title('brush\_fire.mp3');
-% 
-% %Fire_1.mp3 spectrogram
-% [y,Fs] = audioread('fire_sounds/Fire_1.mp3');
-% y = y(:);
-% subplot(2,2,3);
-% spectrogram(y)
-% title('Fire\_1.mp3');
-% 
-% %Forest_Fire_1.mp3 spectrogram
-% [y,Fs] = audioread('fire_sounds/Forest_Fire_1.mp3');
-% y = y(:);
-% subplot(2,2,4);
-% spectrogram(y)
-% title('Forest\_Fire\_1.mp3');
-
-figure(2)
-[y,Fs] = audioread('forrest_sounds/Forest-ambience.mp3');
-y = y(:);
-subplot(1,1,1);
-spectrogram(y)
-title('Forest-ambience.mp3');
-
-%% Frequency Domain Audio Signal Analyzing Testing
-figure(3)
-
-%forestfire.mp3 FFT
-[y,Fs] = audioread('fire_sounds/forestfire.mp3');
-y = y(:);
-y = fft(y);
-nfft = length(y);
-x = Fs./2.*[-1:2./nfft:1-2./nfft];
-subplot(2,2,1);
-plot(x,abs(y));
-title('forestfire.mp3');
-xlabel('Frequency (Hz)');
-ylabel('Magnitude');
-
-%brush_fire.mp3 FFT
-[y,Fs] = audioread('fire_sounds/brush_fire.mp3');
-y = y(:);
-y = fft(y);
-nfft = length(y);
-x = Fs./2.*[-1:2./nfft:1-2./nfft];
-subplot(2,2,2);
-plot(x,abs(y));
-title('brush\_fire.mp3');
-xlabel('Frequency (Hz)');
-ylabel('Magnitude');
-
-%Fire_1.mp3 FFT
-[y,Fs] = audioread('fire_sounds/Fire_1.mp3');
-y = y(:);
-y = fft(y);
-nfft = length(y);
-x = Fs./2.*[-1:2./nfft:1-2./nfft];
-subplot(2,2,3);
-plot(x,abs(y));
-title('Fire\_1.mp3');
-xlabel('Frequency (Hz)');
-ylabel('Magnitude');
-
-%Forest_Fire_1.mp3 FFT
-[y,Fs] = audioread('fire_sounds/Forest_Fire_1.mp3');
-y = y(:);
-y = fft(y);nfft = length(y);
-x = Fs./2.*[-1:2./nfft:1-2./nfft];
-subplot(2,2,4);
-plot(x,abs(y));
-title('Forest\_Fire\_1.mp3');
-xlabel('Frequency (Hz)');
-ylabel('Magnitude');
-
-figure(4)
 
 %Forest-ambience.mp3 FFT
-[y,Fs] = audioread('forrest_sounds/Forest-ambience.mp3');
-y = y(:);
-y = fft(y);nfft = length(y);
-x = Fs./2.*[-1:2./nfft:1-2./nfft];
-plot(x,abs(y));
-title('Forest-ambience.mp3');
-xlabel('Frequency (Hz)');
-ylabel('Magnitude');
+% [y,Fs] = audioread('forrest_sounds/Forest-ambience.mp3');
+% y = y(:);
+% y = fft(y);nfft = length(y);
+% x = Fs./2.*[-1:2./nfft:1-2./nfft];
+% plot(x,abs(y));
+% title('Forest-ambience.mp3');
+% xlabel('Frequency (Hz)');
+% ylabel('Magnitude');
