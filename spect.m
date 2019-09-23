ff257546e1acd8212b26342f98b61338a28d90b2
Cole Hattler <cwh31@duke.edu>
2019-09-23 13:48:26 -0400

    Added frequency and time plots for forest audio signals

--- spect.m
+++ spect.m
@@ -5,7 +5,7 @@ fireFolder = 'fire_sounds/';
 forestDir = 'forest_sounds/*.mp3';
 forestFolder = 'forest_sounds/';
 
-%Spectrograms, Frequency Domain and Time Domains of All Fire Audio Files
+%% Spectrograms, Frequency Domain and Time Domains of All Fire Audio Files
 files = dir(fireDir)
 N = length(files)
 count = 1;
@@ -41,27 +41,37 @@ for i = 1:N
     title(name,'Interpreter', 'none');
 end
 
-%Spectrograms of All Forest Audio Files
-files = dir(forestDir)
-N = length(files)
+%% Spectrograms, Frequency Domain and Time Domains of All Forest Audio Files
+files = dir(forestDir);
+N = length(files);
 for i = 1:N
-    name = files(i).name
-    fileDir = [forestFolder name]
+    name = files(i).name;
+    fileDir = [forestFolder name];
     [y,Fs] = audioread(fileDir);
     y = y(:);
     figure(count);
     count = count+1;
+    
+    %Spectrogram
+    subplot(1,3,1);
     spectrogram(y)
     title(name,'Interpreter', 'none');
+    
+    %Frequency Domain
+    yFFT = fft(y);
+    nfft = length(yFFT);
+    x = Fs./2.*[-1:2./nfft:1-2./nfft];
+    subplot(1,3,2);
+    plot(x,abs(yFFT));
+    title(name,'Interpreter', 'none');
+    xlabel('Frequency (Hz)');
+    ylabel('Magnitude');
+    
+    %Time Domain
+    x = 0:length(y)-1;
+    subplot(1,3,3);
+    plot(x,y);
+    xlabel('Samples');
+    ylabel('Magnitude');
+    title(name,'Interpreter', 'none');
 end
-
-
-%Forest-ambience.mp3 FFT
-% [y,Fs] = audioread('forrest_sounds/Forest-ambience.mp3');
-% y = y(:);
-% y = fft(y);nfft = length(y);
-% x = Fs./2.*[-1:2./nfft:1-2./nfft];
-% plot(x,abs(y));
-% title('Forest-ambience.mp3');
-% xlabel('Frequency (Hz)');
-% ylabel('Magnitude');
