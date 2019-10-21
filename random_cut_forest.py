f2778eabbd6ec01f48b698464110aa3e255bc6ff
Scott McConnell <skm44@duke.edu>
2019-10-21 19:32:29 -0400

    RCF working and printing for sample data

---
+++ random_cut_forest.py
@@ -0,0 +1,46 @@
+# Check https://github.com/kLabUM/rrcf for setup
+import numpy as np
+import pandas as pd
+import rrcf
+import matplotlib.pyplot as plt
+
+# Set parameters
+np.random.seed(0)
+n = 2010
+d = 3
+num_trees = 100
+tree_size = 256
+
+# Generate data
+X = np.zeros((n, d))
+X[:1000,0] = 5
+X[1000:2000,0] = -5
+X += 0.01*np.random.randn(*X.shape)
+
+# Construct forest
+forest = []
+while len(forest) < num_trees:
+    # Select random subsets of points uniformly from point set
+    ixs = np.random.choice(n, size=(n // tree_size, tree_size),
+                           replace=False)
+    # Add sampled trees to forest
+    trees = [rrcf.RCTree(X[ix], index_labels=ix) for ix in ixs]
+    forest.extend(trees)
+
+# Compute average CoDisp
+avg_codisp = pd.Series(0.0, index=np.arange(n))
+index = np.zeros(n)
+for tree in forest:
+    codisp = pd.Series({leaf : tree.codisp(leaf) for leaf in tree.leaves})
+    avg_codisp[codisp.index] += codisp
+    np.add.at(index, codisp.index.values, 1)
+avg_codisp /= index
+
+plt.plot(avg_codisp)
+
+plt.xlabel('Datapoint Index')
+plt.ylabel('Anomaly Score (codisp)')
+
+plt.title("Anomaly Scores in Sample Forest")
+
+plt.show()
