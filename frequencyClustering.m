d43f8df0787df68b61598677235b275896af149e
Brian Jordan <bjj17@duke.edu>
2019-10-21 21:05:00 -0400

    Removed old csv files and added frequencyClustering file

---
+++ frequencyClustering.m
@@ -0,0 +1,7 @@
+%% Frequency clustering algorithm
+% Requires CSV files containing matrices corresponding to fire and non-fire
+% audio file frequency spectrums
+%
+% Rows will indicate different frequency level, columns will indicate
+% normalized magnitudes at the specific frequency for audio clips
+
